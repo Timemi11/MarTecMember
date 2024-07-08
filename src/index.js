93bce58bc038a5346eef9dd42d5cfb61f1bea2b0
Timemi11 <momuu56@gmail.com>
2024-07-08 21:04:27 +0700

    upd index excelDate checkDatetoRealDate

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const xlsx = require('xlsx');
 const admin = require('firebase-admin');
 const fs = require('fs');
 const path = require('path');
+const { excelDateToJSDate } = require('./utils/excelDate.js');
 const serviceAccount = require('./documents/firebase/testmartecmember-2f61fe5f675f.json');
 // ไฟล์อยู่ documents/firebase/filename.json
 
@@ -12,30 +13,50 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
+
+
 // Validation pathfile Excel 
 const filePath = path.join(__dirname, 'documents', 'excelfile', 'member.xlsx');
+
+
 
   if (fs.existsSync(filePath)) {
     // อ่านไฟล์ Excel
     const workbook = xlsx.readFile(filePath);
     const sheet_name_list = workbook.SheetNames;
     const worksheet = workbook.Sheets[sheet_name_list[0]]; // อ่าน sheet แรก
-    const data = xlsx.utils.sheet_to_json(worksheet);
+    const data = xlsx.utils.sheet_to_json(worksheet,{ defval: "" });
+
+
+
+    const formatData = data.map(row => {
+      const newRow = {};
+      for (let key in row) {
+        if (row.hasOwnProperty(key)) {
+          let value = row[key];
+          if ( (key === 'startDate' && value) || (key === 'endDate' && value) || (key === 'paymentDate' && value)  ) {
+            value = excelDateToJSDate(value); // แปลงวันที่
+          }
+          newRow[key] = String(value); // แปลงค่าเป็นสตริง
+        }
+      }
+      return newRow;
+    });
 
 
     // อัปโหลดข้อมูลไปยัง Firebase
-    data.forEach(async (row, index) => {
-      db.collection('members').add(row).then((docRef) => {
+    formatData.forEach(async (row) => {
+      try {
+        const docRef = await db.collection('members').add(row);
+        await db.collection('users').doc("ULineId"+docRef.id).set({ businessId: docRef.id });
         console.log('Document written with ID: ', docRef.id);
-      }).catch((error) => {
+      } catch (error) {
         console.error('Error adding document: ', error);
-      });
+      }
     });
 
 
     console.log('อัปโหลดข้อมูลเรียบร้อยแล้ว');
-
-
 
   } else {
   console.error(`File not found: ${filePath}`);
---
+++ src/utils/excelDate.js
@@ -0,0 +1,13 @@
+ function excelDateToJSDate(serial) {
+    const utc_days = Math.floor(serial - 25569);
+    const utc_value = utc_days * 86400;
+    const date_info = new Date(utc_value * 1000);
+  
+    const year = date_info.getUTCFullYear();
+    const month = ("0" + (date_info.getUTCMonth() + 1)).slice(-2);
+    const day = ("0" + date_info.getUTCDate()).slice(-2);
+  
+    return `${year}-${month}-${day}`;
+  }
+
+  module.exports = { excelDateToJSDate };
