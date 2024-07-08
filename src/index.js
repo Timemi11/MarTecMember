b5acf87fd4fdf17c3fd23cb164a05305c54db26b
Timemi11 <momuu56@gmail.com>
2024-07-08 20:05:08 +0700

    upd version node

--- src/index.js
+++ src/index.js
@@ -14,30 +14,35 @@ const db = admin.firestore();
 
 // Validation pathfile Excel 
 const filePath = path.join(__dirname, 'documents', 'excelfile', 'member.xlsx');
-let data
-
 
   if (fs.existsSync(filePath)) {
     // อ่านไฟล์ Excel
     const workbook = xlsx.readFile(filePath);
     const sheet_name_list = workbook.SheetNames;
     const worksheet = workbook.Sheets[sheet_name_list[0]]; // อ่าน sheet แรก
-     data = xlsx.utils.sheet_to_json(worksheet);
+    const data = xlsx.utils.sheet_to_json(worksheet);
+
+
+    // อัปโหลดข้อมูลไปยัง Firebase
+    data.forEach(async (row, index) => {
+      db.collection('members').add(row).then((docRef) => {
+        console.log('Document written with ID: ', docRef.id);
+      }).catch((error) => {
+        console.error('Error adding document: ', error);
+      });
+    });
+
+
+    console.log('อัปโหลดข้อมูลเรียบร้อยแล้ว');
+
+
+
   } else {
   console.error(`File not found: ${filePath}`);
   }
 
 
-// อัปโหลดข้อมูลไปยัง Firebase
-data.forEach(async (row, index) => {
-  db.collection('members').add(row).then((docRef) => {
-    console.log('Document written with ID: ', docRef.id);
-  }).catch((error) => {
-    console.error('Error adding document: ', error);
-  });
-});
 
-console.log('อัปโหลดข้อมูลเรียบร้อยแล้ว');
 
 
 
