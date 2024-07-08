b5fb78b741f6a6b615fb9fd61e50fa1eeb96544c
Timemi11 <momuu56@gmail.com>
2024-07-08 15:58:01 +0700

    upd gitignore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-
 const xlsx = require('xlsx');
 const admin = require('firebase-admin');
-const serviceAccount = require('');
+const fs = require('fs');
+const path = require('path');
+const serviceAccount = require('./documents/firebase/testmartecmember-2f61fe5f675f.json');
 // ไฟล์อยู่ documents/firebase/filename.json
 
 // เริ่มต้น Firebase Admin SDK
@@ -11,17 +12,25 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-// อ่านไฟล์ Excel
-const workbook = xlsx.readFile('./documents/firebase/testmartecmember-firebase-adminsdk-4ofkj-7de40644aa.json');
-const sheet_name_list = workbook.SheetNames;
-const worksheet = workbook.Sheets[sheet_name_list[0]]; // อ่าน sheet แรก
-const data = xlsx.utils.sheet_to_json(worksheet);
+// Validation pathfile Excel 
+const filePath = path.join(__dirname, 'documents', 'excelfile', 'member.xlsx');
+
+  if (fs.existsSync(filePath)) {
+    // อ่านไฟล์ Excel
+    const workbook = xlsx.readFile(filePath);
+    const sheet_name_list = workbook.SheetNames;
+    const worksheet = workbook.Sheets[sheet_name_list[0]]; // อ่าน sheet แรก
+    const data = xlsx.utils.sheet_to_json(worksheet);
+  } else {
+  console.error(`File not found: ${filePath}`);
+  }
+
 
 // อัปโหลดข้อมูลไปยัง Firebase
-data.forEach(async (row, index) => {
-  const docRef = db.collection('members').doc(String(index));
-  await docRef.set(row);
-});
+// data.forEach(async (row, index) => {
+//   const docRef = db.collection('members').doc(String(index));
+//   await docRef.set(row);
+// });
 
 console.log('อัปโหลดข้อมูลเรียบร้อยแล้ว');
 
